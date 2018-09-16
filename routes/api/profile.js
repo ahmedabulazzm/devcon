db3ede80f7d48410eda8e016973ded70ef8d2d1f
ahmedabulazzm <ahmed.abulazm1@gmail.com>
2018-09-16 02:13:05 +0200

    Updated profile.js route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load DB models
+const ProfileModel = require("../../models/ProfileModel");
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "Profile Works!" }));
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    ProfileModel.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
 
 module.exports = router;
