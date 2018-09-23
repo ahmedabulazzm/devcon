f6c648f427335c2b4f9c734bbf6266f9aed119a7
ahmedabulazzm <ahmed.abulazm1@gmail.com>
2018-09-23 13:29:58 +0200

    Front End

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <footer className="bg-dark text-white text-center mt-5 p-4">
+        Copyright &copy; {new Date().getFullYear()}
+      </footer>
+    </div>
+  );
+};
